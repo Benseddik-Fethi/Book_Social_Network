e435904345501c746fd63978daf3ae2a7d39e2b1
benseddik fethi <fe.benseddik@gmail.com>
2024-06-19 08:44:33 +0200

    Add keycloak

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -1,13 +1,22 @@
-import {ApplicationConfig} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {httpTokenInterceptor} from "./services/interceptor/http-token.interceptor";
-
+import {KeycloakService} from "./services/keycloak/keycloak.service";
+export function kcFactory(kc: KeycloakService) {
+  return () => kc.init();
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([httpTokenInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      deps: [KeycloakService],
+      useFactory: kcFactory,
+      multi: true
+    }
   ]
 };
--- book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -1,10 +1,10 @@
 import {HttpHeaders, HttpInterceptorFn} from '@angular/common/http';
 import {inject} from "@angular/core";
-import {TokenService} from "../token/token.service";
+import {KeycloakService} from "../keycloak/keycloak.service";
 
 export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const tokenService = inject(TokenService);
-  const token = tokenService.token;
+  const keycloakService = inject(KeycloakService);
+  const token = keycloakService.keycloak.token;
   if (token) {
     const authRequest = req.clone({
       headers: new HttpHeaders({
